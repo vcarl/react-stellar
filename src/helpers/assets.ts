efd488c1f50ed346387cfa4bdfef04e160581d5c
Carl Vitullo <carl@lightyear.io>
2018-07-30 08:47:51 -0400

    Add asset parser/type

---
+++ src/helpers/assets.ts
@@ -0,0 +1,16 @@
+import { RawAsset, Asset } from '../../types/stellar';
+
+export const parseAsset = (asset: RawAsset): Asset => {
+  if (asset.asset_type === 'native') {
+    return {
+      assetType: asset.asset_type,
+      assetCode: 'XLM',
+      assetIssuer: 'Stellar',
+    };
+  }
+  return {
+    assetType: asset.asset_type,
+    assetCode: asset.asset_code,
+    assetIssuer: asset.asset_issuer,
+  };
+};
--- types/stellar.d.ts
+++ types/stellar.d.ts
@@ -1,5 +1,20 @@
 export type IssuedAssetType = 'credit_alphanum4' | 'credit_alphanum12';
 export type NativeAssetType = 'native';
+export interface NativeAsset {
+  asset_type: NativeAssetType;
+}
+export interface RawIssuedAsset {
+  asset_type: IssuedAssetType;
+  asset_issuer: string;
+  asset_code: string;
+}
+export type RawAsset = NativeAsset | RawIssuedAsset;
+
+export interface Asset {
+  assetType: IssuedAssetType | NativeAssetType;
+  assetIssuer: string;
+  assetCode: string;
+}
 
 interface IssuedBalance {
   balance: string;
