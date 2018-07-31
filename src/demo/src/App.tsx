4b8e5a3ed7008eae32971f41987eed9347ab2f36
Carl Vitullo <carl@lightyear.io>
2018-07-31 18:48:50 -0400

    Demo

---
+++ src/demo/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return <div className="App">Butts</div>;
+  }
+}
+
+export default App;
